addca4a77a77afa13ba43bb3e417707d54923e17
shuipingmian <www.15350014683@163.com>
2020-02-01 01:50:05 +0800

    vue 饿了么  tab 组件和goods组件

---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="rating">
+    rating
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rating',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () { },
+  mounted () { }
+}
+</script>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/seller/seller.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="seller">
+    seller
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'seller',
+  components: {},
+  props: {},
+  data () {
+    return {
+    }
+  },
+  watch: {},
+  computed: {},
+  methods: {},
+  created () { },
+  mounted () { }
+}
+</script>
+<style lang="stylus" scoped>
+.seller
+   height: 200px;
+</style>
